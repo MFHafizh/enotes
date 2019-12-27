a85a4db2c08f50379dac56afba54ae3fb4079802
Muhammad Fakhruddin Hafizh <hafizh203@gmail.com>
2019-12-27 21:41:33 +0700

    implement OpenGL on splash screen

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammadfakhruddinhafizh/enotes/SplashActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/muhammadfakhruddinhafizh/enotes/SplashActivity.java
@@ -1,21 +1,48 @@
 package id.ac.ui.cs.mobileprogramming.muhammadfakhruddinhafizh.enotes;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
-/**
- * Created by AbhiAndroid
- */
+import id.ac.ui.cs.mobileprogramming.muhammadfakhruddinhafizh.enotes.openGL.LessonOneRenderer;
+
 
 public class SplashActivity extends Activity {
-
+    /** Hold a reference to our GLSurfaceView */
+    private GLSurfaceView mGLSurfaceView;
     Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Turn off the window's title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_file);
+        mGLSurfaceView = new GLSurfaceView(this);
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+        if (supportsEs2)
+        {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(2);
+
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(new LessonOneRenderer());
+        }
+        else
+        {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
+        setContentView(mGLSurfaceView);
 
         handler=new Handler();
         handler.postDelayed(new Runnable() {
@@ -26,6 +53,19 @@ public class SplashActivity extends Activity {
                 finish();
             }
         },3000);
+    }
 
+    @Override
+    protected void onResume(){
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+    @Override
+    protected void onPause()
+    {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        //mGLSurfaceView.onPause();
     }
 }
